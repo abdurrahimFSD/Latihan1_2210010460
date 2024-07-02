34148839c2d724706fd6f8e0163fd2912e9333a6
Abdurrahim <abdurrahim22code@gmail.com>
2024-07-02 16:30:07 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // Object
+        Matematika ahim = new Matematika(6, 0);
+        
+        System.out.println("Penjumlahan: " + ahim.setPenjumlahan());
+        System.out.println("Pengurangan: " + ahim.setPengurangan());
+        System.out.println("Perkalian: " + ahim.setPerkalian());
+        System.out.println("Pembagian: " + ahim.setPembagian());
+    }
 }
